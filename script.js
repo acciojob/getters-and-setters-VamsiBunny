52fd7ad65fce2d5674099385e7fde1a502af3330
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-10-28 15:47:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	super(name,age)
+ get name(){
+	name="Vamsi";
+ }
+set age(){
+	age=21;
+}
+ study(){
+  console.log(name+" is studying");
+ }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	super(name,age)
+teach(){
+console.log(name+" is teaching");
+}
+}
+
 
 // Do not change the code below this line
 window.Person = Person;

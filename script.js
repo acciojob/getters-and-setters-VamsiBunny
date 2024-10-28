22af30155df371ce3a5ea2bd5301dc1e6b19a734
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-10-28 16:00:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
-//complete this code
 class Person {
-	constuctor(name,age){
-      this.name=name;
-	  this.age=age;
-	}
-	
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set age(newAge) {
+        this._age = newAge;
+    }
+
+    get age() {
+        return this._age;
+    }
 }
 
 class Student extends Person {
-	constuctor(name,age){
-		super(name,age);
-	}
-get name(){
-return this.name;
-}
-set age(newAge){
-	this.age=newAge;
-} 
-study(){
-return this.name+" is studying";
-}
-	
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    study() {
+        console.log(this.name + " is studying");
+    }
 }
 
 class Teacher extends Person {
-	constuctor(name,age){
-		super(name,age);
-	}
-	teach(){
-      return this.name+" is teaching";
-	}
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    teach() {
+        console.log(this.name + " is teaching");
+    }
 }
 
-
-// Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
